3b20470cb366231c334765e899375d5f56b3a169
{nomaday} <{rzn9619@gmail.com}>
2022-05-06 13:23:21 +0900

    assignment 1

--- hello.py
+++ hello.py
@@ -1,1 +1,18 @@
+
+# 과제: 1부터 20까지 반복하는 과정에서 3의 배수일 경우, year를 출력하시오.
+# 추가 과제: 5의 배수일 때, draem 출력
+# 추가 과제2: 15의 배수일 때, yeardream
+# 나머지 모든 경우는 숫자 그대로 출력
+
+for i in range(10):
+    if i%3 == 0:
+        print('year')
+    else: 
+        print(i)
+
+# $ vi hello.py
+# $ git status
+# $ git add hello.py
+# $ git commit
+
 print('hello')
