1c57650c8e5322340a1e136dcea658edc535a99e
{nomaday} <{rzn9619@gmail.com}>
2022-05-06 13:34:11 +0900

    assignment 3

--- hello.py
+++ hello.py
@@ -4,8 +4,10 @@
 # 추가 과제2: 15의 배수일 때, yeardream
 # 나머지 모든 경우는 숫자 그대로 출력
 
-for i in range(10):
-    if i%3 == 0:
+for i in range(0, 20+1):
+    if i%15 == 0:
+        print('yeardream')
+    elif i%3 == 0:
         print('year')
     elif i%5 == 0:
         print('dream')
